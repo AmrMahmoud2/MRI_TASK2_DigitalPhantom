b23459c4977ef56a8308a917f685bb4bea983a6c
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2019-03-23 04:15:05 +0200

    "Plotting T1 and T2 With different colors for each pixel"

--- phantom-test.py
+++ phantom-test.py
@@ -3,13 +3,14 @@ import sys
 from copy import deepcopy
 
 from PIL.ImageQt import ImageQt
-from PyQt5.QtWidgets import QInputDialog, QFileDialog, QMessageBox, QSizePolicy,QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QInputDialog, QFileDialog, QMessageBox, QSizePolicy, QVBoxLayout, QSizePolicy,QDockWidget,QMainWindow
 from PIL import Image
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import QApplication,QDialog
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from pyqtgraph import PlotWidget
 from qimage2ndarray import gray2qimage
 import sys
 import numpy as np
@@ -28,19 +29,22 @@ fg_color = 'white'
 
 
 
-class MRI(QDialog):
+class MRI(QMainWindow ):
     def __init__(self):
         super(MRI,self).__init__()
-        loadUi('MRI-Phantom.ui',self)
+        loadUi('MRI-Phantom2.ui',self)
         self.setWindowTitle('MRI-Phantom')
         #self.b1.clicked.connect(self.on_click)
         self.cb.activated[str].connect(self.temp_var)
-        #self.graphicsView = pg.PlotWidget()
-        #self.m = PlotCanvas(self, width=15, height=6)
-        #self.m.move(168, 410)
-        self.m=DynamicPlotter(sampleinterval=0.05, timewindow=20.)
+        self.GraphicsView()
+        # self.r=255
+        # self.g=0
+        # self.b=0
+        self.colors =self.get_spaced_colors(5)
+        print(self.colors)
+        self.i=0
 
-        self.current_width=self.l1.width()
+
         #self.setAcceptHoverEvents(True)
 
 
@@ -55,61 +59,55 @@ class MRI(QDialog):
 
 
 
-    def Display(self,model_no,size):
-        array = self.Phantom(model_no,size)
-        img = self.convertArrayToImage(array)
-        pixmap=self.ShowImage(img)
-        self.l1.setPixmap(pixmap)
-        #Mouse Event
-        self.l1.mousePressEvent = self.getPixel
-        array_with_time=self.setTime(img)
-        # construct t1 image
-        self.array_t1=self.Return(array_with_time,1)
-        img_t1=self.convertArrayToImage(self.array_t1)
-        pixmap_t1=self.ShowImage(img_t1)
-        #self.l2.setPixmap(pixmap_t1)
-        #construct t2 image
-        self.array_t2 = self.Return(array_with_time, 2)
-        img_t2 = self.convertArrayToImage(self.array_t2)
-        pixmap_t2 = self.ShowImage(img_t2)
-        self.l3.setPixmap(pixmap_t2)
-        #highlight trial
-        # highlight_arry=self.convertImageToArray(img)
-        # highlight_img_RGB = Image.fromarray(highlight_arry).convert('RGB')
-        # test_array=self.convertImageToArray(highlight_img_RGB)
-        # test_array=np.array(test_array)
-        # test_array[:, :3] = [255, 128, 0]
-        # highlight_img=self.convertArrayToImage(test_array)
-        # pixmap_test=self.ShowImage(highlight_img)
-        # self.l3_2.setPixmap(pixmap_test)
-        I = np.dstack([img, img, img])
-        x = 73
-        y = 75
-        I[x, y, :] = [255, 128, 0]
-        highlight_img= Image.fromarray(I).convert('RGB')
-        pixmap_test=self.ShowImage(highlight_img)
-        self.l2.setPixmap(pixmap_test)
-
-
-
-
-
-
-
-
-
 
 
     def temp_var(self):
         cur_txt = self.cb.currentText()
         if cur_txt == "Shepp-Logan phantom":
-            self.Display(14,self.current_width)
+            self.Display(14,self.l1.width())
         else:
             self.l1.hide()
 
+
+
+
+
+
+
+
+
+
+    def Display(self,model_no,size):
+        array = self.Phantom(model_no, size)
+        self.img = self.convertArrayToImage(array)
+        pixmap = self.ShowImage(self.img)
+        self.l1.setPixmap(pixmap)
+        # Mouse Event
+        self.l1.mousePressEvent = self.getPixel
+        array_with_time = self.setTime(self.img)
+        # construct t1 image
+        self.array_t1 = self.Return(array_with_time, 1)
+        img_t1 = self.convertArrayToImage(self.array_t1)
+        pixmap_t1 = self.ShowImage(img_t1)
+        self.l2.setPixmap(pixmap_t1)
+        # construct t2 image
+        self.array_t2 = self.Return(array_with_time, 2)
+        img_t2 = self.convertArrayToImage(self.array_t2)
+        pixmap_t2 = self.ShowImage(img_t2)
+        self.l3.setPixmap(pixmap_t2)
+        print(self.l1.width())
+        # I = np.dstack([img, img, img])
+        # x = 73
+        # y = 75
+        # I[x, y, :] = [255, 128, 0]
+        # highlight_img= Image.fromarray(I).convert('RGB')
+        # pixmap_test=self.ShowImage(highlight_img)
+        # self.l2.setPixmap(pixmap_test)
+
+
     def Phantom(self,model,size):
      model =model
-     sizeN = 1080
+     sizeN = 256
      #This will generate 256x256 phantom from the model
      self.phantom = TomoP2D.Model(model, sizeN,'/home/sohila/Documents/BIO-MATERIALS/Third-Year/Second-Term/MRI/Task2-MRI/phantoms/TomoPhantom/PhantomLibrary/models/Phantom2DLibrary.dat')
      angles_num = int(0.5*np.pi*sizeN) # angles number
@@ -195,22 +193,68 @@ class MRI(QDialog):
         return d2
 
     def getPixel (self, event):
-        if self.current_width != self.l1.width():
-            self.current_width = self.l1.width()
-            self.Display(14,self.current_width)
         self.x = event.pos().x()
         self.y = event.pos().y()
+        print('before',self.x, self.y)
+        self.ratio = 256 / self.l1.width()
+        if(self.x>256):
+
+            self.x=int(self.ratio*self.x)
+
+        if (self.y > 256):
+            self.y = int(self.ratio * self.y)
+
         print("t1",self.array_t1[self.y][self.x])
         print("t2",self.array_t2[self.y][self.x])
         print(self.x,self.y)
-        # plotting
-        #self.m.plot(self.array_t1[self.y][self.x], self.array_t2[self.y][self.x])
+        if(self.i<5):
+          self.plotting(self.array_t1[self.y][self.x],self.array_t2[self.y][self.x])
+          self.tabWidget.setCurrentIndex(1)
         print(self.l1.width())
-    #
-    # def hoverEnterEvent(self, event):
-    #     self.setBrush(Qt.yellow)
-    #     QDialog.hoverEnterEvent(self, event)
 
+
+    def plotting(self,T1,T2):
+        # define the data
+        self.x = np.linspace(0, 1000, 400)
+        self.x2 = np.linspace(0, 1000, 400)
+        self.y = 1 - np.exp((-self.x) / T1)
+        self.y2 = np.exp((-self.x2) / T2)
+        # plot
+        self.c = self.plt.plot(self.x, self.y, pen=self.colors[self.i], name=('pixel'))
+        self.c2 = self.plt2.plot(self.x2, self.y2, pen=self.colors[self.i], name='pixel')
+        self.i=self.i+1
+    def get_spaced_colors(self,n):
+        max_value = 16581375  # 255**3
+        interval = int(max_value / n)
+        colors = [hex(I)[2:].zfill(6) for I in range(200, max_value, interval)]
+
+        return [(int(i[:2], 16), int(i[2:4], 16), int(i[4:], 16)) for i in colors]
+
+
+
+
+    def GraphicsView (self):
+        # create plot
+        self.plt = pg.PlotItem()
+        self.plt2 = pg.PlotItem()
+        self.plt.showGrid(x=True, y=True)
+        self.plt.addLegend()
+        self.plt2.showGrid(x=True, y=True)
+        self.plt2.addLegend()
+
+        # set properties
+        self.plt.setLabel('left', 'Value', units='MZ')
+        self.plt.setLabel('bottom', 'Time', units='s')
+        self.plt.setTitle('T1 Recovery')
+        self.plt2.setLabel('left', 'Value', units='MZ')
+        self.plt2.setLabel('bottom', 'Time', units='s')
+        self.plt2.setTitle('T2 Decay')
+
+        # set Graphics View
+
+        self.graphicsView.showGrid(x=True, y=True)
+        self.graphicsView.setCentralItem(self.plt)
+        self.graphicsView_2.setCentralItem(self.plt2)
 
 
 
@@ -224,101 +268,12 @@ class MRI(QDialog):
         return array2
 
 
-class PlotCanvas(FigureCanvas):
-
-    def __init__(self, parent=None, width=5, height=5, dpi=50):
-        fig = Figure(figsize=(width, height), dpi=dpi,facecolor='black')
-
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-        self.parentWindow = parent
-
-        FigureCanvas.setSizePolicy(self,QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
 
 
 
-    def plot(self,T1,T2):
-         #T1 plotting
-         fig = plt.figure(facecolor=bg_color, edgecolor=fg_color)
-
-         x = np.linspace(0, 10000, 100)
-         y = 1.5*(1-(np.exp((-x)/T1)))
-         ax = self.figure.add_subplot(121)
-         ax.patch.set_facecolor(bg_color)
-         ax.xaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
-         ax.yaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
-         for spine in ax.spines.values():
-             spine.set_color(fg_color)
-         ax.plot(x, y,'cyan', axes=ax)
-         plt.xlabel('Time', color=fg_color)
-         plt.ylabel('T1', color=fg_color)
-         ax.set_title('T1',color=fg_color)
-         #T2 Plotting
-         x2 = np.linspace(0, 10000, 100)
-         y2 = 1.5*(np.exp((-x2)/T2))
-         ax2 = self.figure.add_subplot(122)
-         ax2.patch.set_facecolor(bg_color)
-         ax2.xaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
-         ax2.yaxis.set_tick_params(color=fg_color, labelcolor=fg_color)
-         for spine in ax2.spines.values():
-             spine.set_color(fg_color)
-         ax2.plot(x2, y2)
-         plt.xlabel('Time', color=fg_color)
-         plt.ylabel('T2', color=fg_color)
-
-         ax2.set_title('T2',color=fg_color)
-         self.draw()
-
-
-class DynamicPlotter():
-
-    def __init__(self, sampleinterval=1, timewindow=100., size=(600,350)):
-        # Data stuff
-        self._interval = int(sampleinterval*1000)
-        self._bufsize = int(timewindow/sampleinterval)
-        self.databuffer = collections.deque([0.0]*self._bufsize, self._bufsize)
-        self.x = np.linspace(timewindow, 0.0, self._bufsize)
-        self.y = np.zeros(self._bufsize, dtype=np.float)
-
-        # PyQtGraph stuff
-        self.app = QtGui.QApplication([])
-        self.window=pg.GraphicsWindow(title="Basic plotting examples")
-        self.window.resizeEvent(size)
-        self.plt = self.window.addPlot(title="Basic array plotting")
-        self.plt.showGrid(x=True, y=True)
-        self.plt.setLabel('left', 'amplitude', 'V')
-        self.plt.setLabel('bottom', 'time', 's')
-        self.curve = self.plt.plot(self.x, self.y, pen=(255,0,0))
-        self.curve = self.plt.plot(self.x, self.y, pen=(255, 0, 0))
-        ##plt2
-        self.plt2 = self.window.addPlot(title="Multiple curves")
-        self.plt2.showGrid(x=True, y=True)
-        self.plt2.setLabel('left', 'amplitude', 'V')
-        self.plt2.setLabel('bottom', 'time', 's')
-        self.curve2 = self.plt2.plot(self.x, self.y, pen=(255, 0, 0))
-        self.curve2 = self.plt2.plot(self.x, self.y, pen=(255, 0, 0))
 
 
 
-        # QTimer
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.updateplot)
-        self.timer.start(self._interval)
-
-    def getdata(self):
-        frequency = 0.5
-        noise = random.normalvariate(0., 1.)
-        new = 10.*math.tan(time.time()*frequency*2*math.pi) + noise
-        return new
-
-    def updateplot(self):
-        self.databuffer.append( self.getdata() )
-        self.y[:] = self.databuffer
-        self.curve.setData(self.x, self.y)
-        self.curve2.setData(self.x, self.y)
-        self.app.processEvents()
 
 
 
