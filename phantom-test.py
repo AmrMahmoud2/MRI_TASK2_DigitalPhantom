3785fa64c66142a4c434ad77190ee0848241813a
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2019-04-11 04:59:32 +0200

    "Update2"

--- phantom-test.py
+++ phantom-test.py
@@ -24,6 +24,7 @@ import collections
 import random
 import time
 import math
+import HelloCython
 
 bg_color = 'black'
 fg_color = 'white'
@@ -57,8 +58,7 @@ class MRI(QMainWindow ):
         # self.b=0
         self.colors =self.get_spaced_colors(5)
         print(self.colors)
-        self.i=0
-        self.bp = 1
+
 
 
 
@@ -111,15 +111,17 @@ class MRI(QMainWindow ):
 
 
     def Display(self,model_no,size):
+        self.i=0
+        self.bp = 1
+        self.ratio_count=1
         array = self.Phantom(model_no, size)
-        self.img = self.convertArrayToImage(array)
-        self.phantom_Pixels_array=np.asarray(self.img)
+        self.nomalized_phantom_array=self.Normalization(array)
+        self.img=self.convertArrayToImage(self.nomalized_phantom_array)
         self.img.save('img.png')
-        qimage = ImageQt(self.img)
-        pixmap = QPixmap.fromImage(qimage)
-        self.pixmap = QPixmap(pixmap)
-        pixmap = self.pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
-        self.l1.setPixmap(pixmap)
+        self.phantom_pixmap=self.ShowImage(self.img)
+        self.Pixels_Array=self.convertImageToArray()
+        phantom_pixmap = self.phantom_pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
+        self.l1.setPixmap(phantom_pixmap)
         # Mouse Event
         self.l1.mouseDoubleClickEvent = self.getPixel
         self.l1.mouseMoveEvent=self.mouseMoveEvent
@@ -127,25 +129,39 @@ class MRI(QMainWindow ):
         self.l1.mousePressEvent = self.getpixels
 
 
+    def TimeMapping(self,time_array):
 
+        #k_space = np.zeros((self.num, self.num), dtype=np.complex)
+        self.T1_Mapped=np.zeros((len(time_array),len(time_array)),dtype=float)
+        max_t1=np.max(time_array)
+        min_t1=np.min(time_array)
+        for i in range (len(time_array)):
+            for j in range (len(time_array)):
+                self.T1_Mapped[i][j]=((time_array[i][j]-min_t1)/(max_t1-min_t1))*255
+        return self.T1_Mapped
 
 
 
 
     def Display_T1(self):
         # construct t1 image
-        self.array_with_time = self.setTime(self.img)
+        self.array_with_time = self.setTime()
         self.array_t1 = self.Return(self.array_with_time, 1)
-        self.img_t1 = self.convertArrayToImage(self.array_t1)
+        t1_mapped=self.TimeMapping(self.array_t1)
+        #self.array_t1 = self.Return(self.array_with_time, 1)
+        self.img_t1 = self.convertArrayToImage(t1_mapped)
         pixmap_t1 = self.ShowImage(self.img_t1)
+        pixmap_t1 = pixmap_t1.scaled(self.l2.width(), self.l2.height(), QtCore.Qt.KeepAspectRatio)
         self.l2.setPixmap(pixmap_t1)
 
     def Display_T2(self):
         # construct t2 image
-        self.array_with_time = self.setTime(self.img)
+        self.array_with_time = self.setTime()
         self.array_t2 = self.Return(self.array_with_time, 2)
-        self.img_t2 = self.convertArrayToImage(self.array_t2)
+        t2_mapped = self.TimeMapping(self.array_t2)
+        self.img_t2 = self.convertArrayToImage(t2_mapped)
         pixmap_t2 = self.ShowImage(self.img_t2)
+        pixmap_t2 = pixmap_t2.scaled(self.l3.width(), self.l3.height(), QtCore.Qt.KeepAspectRatio)
         self.l3.setPixmap(pixmap_t2)
         # I = np.dstack([img, img, img])
         # x = 73
@@ -157,9 +173,18 @@ class MRI(QMainWindow ):
     def Display_K_Space(self):
         #K-Space
         K_array=self.K_Space()
-        img_K = self.convertArrayToImage(K_array)
+        k_space_normalized_array=self.Normalization(K_array)
+        img_K = self.convertArrayToImage(k_space_normalized_array)
         pixmap_k = self.ShowImage(img_K)
-        self.l4.setPixmap(pixmap_k)
+        pixmap_k = pixmap_k.scaled(self.l5.width(), self.l5.height(), QtCore.Qt.KeepAspectRatio)
+        self.l5.setPixmap(pixmap_k)
+        self.Display_K_Fourier()
+    def Display_K_Fourier(self):
+        K_space_fourier_normalized=self.Normalization(self.k_space)
+        img_fourier=self.convertArrayToImage(K_space_fourier_normalized)
+        Fourier_pixmap=self.ShowImage(img_fourier)
+        Fourier_pixmap=Fourier_pixmap.scaled(self.l4.width(), self.l4.height(), QtCore.Qt.KeepAspectRatio)
+        self.l4.setPixmap(Fourier_pixmap)
     def Reset(self):
         qimage = ImageQt('img.png')
         pixmap = QPixmap.fromImage(qimage)
@@ -167,9 +192,28 @@ class MRI(QMainWindow ):
         pixmap = self.pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
         self.l1.setPixmap(pixmap)
 
+    def Normalization (self, arr):
+        # to get rid of dividing bt zero
+        # min_nonzero = np.min(arr[np.nonzero(arr)])
+        # arr[arr == 0] = min_nonzero
 
+        img = np.abs(20 * np.log(arr))
+        return img
 
+    def convertImageToArray(self):
+        # to onvert image to np array of pixels values it should be read as q image at first
+        self.im = Image.open('img.png')
+        arr = np.asarray(self.im)
+        print('this is image pixel function')
+        return arr
 
+    def convertArrayToImage(self, arr):
+        img = gray2qimage(arr)
+        return img
+
+    def ShowImage(self, img):
+        pixmap = QPixmap(img)
+        return pixmap
 
     def Phantom(self,model,size):
      model =model
@@ -192,27 +236,6 @@ class MRI(QMainWindow ):
      # G=TomoP2D.Object(256, pp)
      return self.phantom
 
-    def convertArrayToImage(self, arr):
-        # to get rid of dividing bt zero
-        # min_nonzero = np.min(arr[np.nonzero(arr)])
-        # arr[arr == 0] = min_nonzero
-
-        img = np.abs(20 * np.log(arr))
-        img_back = Image.fromarray(img).convert('L')
-        return img_back
-
-
-    def convertImageToArray(self,img):
-         arr = np.asarray(img)
-         print('this is image pixel function')
-         return arr
-
-    def ShowImage(self, img):
-     qimage = ImageQt(img)
-     pixmap = QPixmap.fromImage(qimage)
-     pixmap = QPixmap(pixmap)
-     pixmap = pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
-     return pixmap
 
     def Construct3DArray(self,Array2d,index):
         d1 = [[[1 for col in range(4)] for row in range(len(Array2d))] for x in range(len(Array2d))]
@@ -227,27 +250,27 @@ class MRI(QMainWindow ):
             for y in range(len(Array_3d)):
                 if (Array_3d[x][y][0]>=0 and Array_3d[x][y][0]<=Min_Value ) :
                     Array_3d[x][y][1] = 250
-                    Array_3d[x][y][2] = 70
+                    Array_3d[x][y][2] = 40
 
                 else:
                     if (Array_3d[x][y][0]>Min_Value and Array_3d[x][y][0]<=AVG):
-                        Array_3d[x][y][1] = 900
+                        Array_3d[x][y][1] = 500
                         Array_3d[x][y][2] = 50
 
                     elif(Array_3d[x][y][0]>AVG and Array_3d[x][y][0]<=Max_value):
 
-                        Array_3d[x][y][1] = 4000
-                        Array_3d[x][y][2] = 2000
+                        Array_3d[x][y][1] = 900
+                        Array_3d[x][y][2] = 90
         return Array_3d
 
-    def setTime(self,image):
-        Pixels_Array = np.asarray(image)
+    def setTime(self):
+        #self.Pixels_Array = np.asarray(image)
         #self.Pixels_Array_Filterd=self.Filters(Pixels_Array,255)
-        Unique_Pixels_values= np.unique(Pixels_Array)
+        Unique_Pixels_values= np.unique(self.Pixels_Array)
         Max_value=np.max(Unique_Pixels_values)
         Min_Value=np.min(Unique_Pixels_values)
-        AVG=np.average(Pixels_Array)
-        Array_3d_With_intensity=self.Construct3DArray(Pixels_Array,0)
+        AVG=np.average(self.Pixels_Array)
+        Array_3d_With_intensity=self.Construct3DArray(self.Pixels_Array,0)
         Array_3d_With_T1_T2=self.Set_T(Array_3d_With_intensity,Max_value,Min_Value,AVG)
         #Array_3d_With_T2 = self.Set_T2(Array_3d_With_T1, Max_value, Min_Value)
         return (Array_3d_With_T1_T2)
@@ -327,15 +350,39 @@ class MRI(QMainWindow ):
         elif x in range(self.x_single - 10, self.x_single + 10) and y> self.y_single and y< self.l1.height():
             self.bp = self.bp - 0.01
             self.Enhancer(self.bp)
+        if(y in range(self.y_single-10 , self.y_single+10 ) and x<self.x_single and x>0 ):
+             self.ratio_count=self.ratio_count-5
+             contrast_img = self.change_contrast(self.im, self.ratio_count)
+             qimage = ImageQt(contrast_img)
+             pixmap = QPixmap.fromImage(qimage)
+             self.phantom_pixmap = QPixmap(pixmap)
+             self.l1.setPixmap(self.phantom_pixmap)
+
+        elif (y in range(self.y_single - 10, self.y_single + 10) and x > self.x_single and x < self.l1.width()):
+            self.ratio_count = self.ratio_count+5
+            contrast_img=self.change_contrast(self.im, self.ratio_count)
+            qimage = ImageQt(contrast_img)
+            pixmap = QPixmap.fromImage(qimage)
+            self.phantom_pixmap = QPixmap(pixmap)
+            self.l1.setPixmap(self.phantom_pixmap)
 
 
     def Enhancer(self,i):
-        enhancer = ImageEnhance.Brightness(self.img)
+        enhancer = ImageEnhance.Brightness(self.im)
         self.enhanced_im = enhancer.enhance(self.bp)
         qimage = ImageQt(self.enhanced_im)
         pixmap = QPixmap.fromImage(qimage)
-        self.pixmap = QPixmap(pixmap)
-        self.l1.setPixmap(pixmap)
+        self.phantom_pixmap = QPixmap(pixmap)
+        self.l1.setPixmap(self.phantom_pixmap)
+
+    def change_contrast(self, img, level):
+        factor = (259 * (level + 255)) / (255 * (259 - level))
+        def contrast(c):
+            value = 128 + factor * (c - 128)
+            return max(0, min(255, value))
+
+        return img.point(contrast)
+
 
 
         print(self.i)
@@ -357,6 +404,7 @@ class MRI(QMainWindow ):
         self.c = self.plt.plot(self.x, self.y, pen=self.colors[self.i], name=('pixel'))
         self.c2 = self.plt2.plot(self.x2, self.y2, pen=self.colors[self.i], name='pixel')
         self.i=self.i+1
+
     def get_spaced_colors(self,n):
         max_value = 16581375  # 255**3
         interval = int(max_value / n)
@@ -405,7 +453,7 @@ class MRI(QMainWindow ):
 
         # create painter instance with pixmap
 
-        self.painterInstance = QtGui.QPainter(self.pixmap)
+        self.painterInstance = QtGui.QPainter(self.phantom_pixmap)
         self.painterInstance.begin(self)
 
         # set rectangle color and thickness
@@ -419,11 +467,13 @@ class MRI(QMainWindow ):
 
         # draw rectangle on painter
         self.painterInstance.setPen(self.penRectangle)
-        self.painterInstance.drawRect(self.x_paint-2.5, self.y_paint-2.5, 5, 5)
+        lx=int(34*self.xratio)
+        ly=int(34*self.yratio)
+        self.painterInstance.drawRect(self.x_paint-(lx/2), self.y_paint-(ly/2), lx, ly)
         print("test",self.x_paint,self.y_paint)
         # set pixmap onto the label widget
 
-        pixmap_image = self.pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
+        pixmap_image = self.phantom_pixmap.scaled(self.l1.width(), self.l1.height(), QtCore.Qt.KeepAspectRatio)
         self.l1.setPixmap(pixmap_image)
         self.l1.show()
         self.painterInstance.end()
@@ -463,30 +513,40 @@ class MRI(QMainWindow ):
 
     def K_Space(self):
 
-        self.tr = 100*np.average(self.array_t1)
-        self.te = 300
+        # self.tr = 100*np.average(self.array_t1)
+        # self.te = 300
         print(self.tr,self.te)
-        k_space = np.zeros((30, 30), dtype=np.complex)
+        self.k_space = np.zeros((self.num, self.num), dtype=np.complex)
 
-        signal =np.ones(30)*np.sin(self.theta*(np.pi/180))
+        signal =np.ones(self.num)*np.sin(self.theta*(np.pi/180))
 
-        for kspacerow in range(30):
+        for kspacerow in range(self.num):
+            QApplication.processEvents()
 
             signal = signal * np.exp(-self.te / self.array_t2)
 
-            for kspacecol in range(30):
-                GX = 2 * np.pi * kspacerow / 30
-                GY = 2 * np.pi * kspacecol / 30
-
-                for i in range(30):
-                    for j in range(30):
+            for kspacecol in range(self.num):
+                GX = 2 * np.pi * kspacerow / self.num
+                GY = 2 * np.pi * kspacecol / self.num
+                QApplication.processEvents()
+                for i in range(self.num):
+                    for j in range(self.num):
                         total_theta = (GX * i + GY * j)
-                        k_space[kspacerow, kspacecol] += signal[i, j] * np.exp(-1j * total_theta)
-
+                        self.k_space[kspacerow, kspacecol] += signal[i, j] * np.exp(-1j * total_theta)
+                        QApplication.processEvents()
             signal = 1 - np.exp(-self.tr / self.array_t1)
 
-        test1 = (np.fft.ifft2(k_space))
+        test1 = (np.fft.ifft2(self.k_space))
+        # k=HelloCython.K_Space(self.tr, self.te,self.theta,self.num,self.array_t1,self.array_t2)
+        # print(k)
         return test1
+    # def K_Space(self):
+    #     self.k_space=HelloCython.K_Space(self.tr, self.te,self.theta,self.num,self.array_t1,self.array_t2)
+    #     test1 = (np.fft.ifft2(self.k_space))
+    #     return test1
+
+
+
 
 
 
